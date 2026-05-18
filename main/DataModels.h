9a47abf9936d03b88a347c3ec256966e40c4ea1d
Akshat <panda.akshat2022wizard@gmail.com>
2026-05-18 21:40:12 +0530

    added struct datatypes for the firmware
    Time struct to store time, FileHeader struct to serve as file system header, Event struct to store event and it's details

---
+++ main/DataModels.h
@@ -0,0 +1,41 @@
+#ifndef DATAMODELS_H
+#define DATAMODELS_H
+
+// ==========================================
+// TIME STRUCTURE (7 Bytes)
+// ==========================================
+typedef struct {
+  uint16_t year;    // 2 bytes
+  uint8_t month;    // 1 byte
+  uint8_t date;     // 1 byte
+  uint8_t hour;     // 1 byte
+  uint8_t min;      // 1 byte
+  uint8_t sec;      // 1 byte
+} __attribute__((packed)) Time;
+
+// ==========================================
+// FILE SYSTEM HEADER (6 Bytes)
+// ==========================================
+typedef struct {
+  uint16_t version;         // 2 bytes
+  uint16_t totalEvents;     // 2 bytes
+  uint16_t currentEventID;  // 2 bytes
+} __attribute__((packed)) FileHeader;
+
+// ==========================================
+// EVENT STRUCTURE (60 Bytes)
+// ==========================================
+typedef struct {
+  uint16_t id;                  // 2 bytes
+  char name[16];                // 16 bytes
+  char details[32];             // 32 bytes
+  Time eventTime;               // 7 bytes
+  
+  // BITMASK: [7-6: State] | [5-3: Repeat Type] | [2-0: Reminder Type]
+  uint8_t flags;                // 1 byte 
+  
+  uint8_t repeatInterval;       // 1 byte
+  uint8_t customRepeatDays;     // 1 byte (Bitmask for Mon-Sun)
+} __attribute__((packed)) Event;
+
+#endif
