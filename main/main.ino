3672de44ab9c45eb994ec259a3e97f711057990c
Akshat Panda <panda.akshat2022wizard@gmail.com>
2026-06-08 08:46:36 +0530

    testing the SystemCtrl class

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,11 @@
-void setup() {
+#include "SystemCtrl.h"
 
+void setup() {
+    SystemCtrl sysctl(15000);
+    sysctl.init();
+    sysctl.boot_handler();
+    sysctl.system_loop();
+    sysctl.shutdown_handler();
 }
 
 void loop() {
